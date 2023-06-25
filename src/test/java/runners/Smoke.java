7842808520f68dbcfdbb88c7b1c065b39321d838
ruslan_v <mailruslan1@gmail.com>
2023-06-25 14:23:23 -0400

    merge

--- src/test/java/runners/Smoke.java
+++ src/test/java/runners/Smoke.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources/features/",
         glue = "steps",
-        dryRun = false,
+        dryRun = true,
         monochrome = true,
 
         tags = "@smoke",
